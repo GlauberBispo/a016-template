e0b402f04fb3e40d55a214938cbb1a1e48a1c811
GlauberBispo <glauber.bispo@gmail.com>
2022-08-17 21:19:37 -0300

    exercío aula 016 laços 2

--- script.js
+++ script.js
@@ -35,3 +35,17 @@ const filmes = [
 ]
 
 // escreva seu código abaixo 👇🏻
+
+// Exercício de prática
+
+console.log(filmes[0].elenco[0].length / 3)
+
+for (let i = 0; i < filmes.length; i++) {
+  let string = `"${filmes[i].titulo}, de ${filmes[i].ano}, dirigido por ${filmes[i].diretor},
+Tem no elenco: "`
+   for (let j = 0; j < filmes[i].elenco[i].length / filmes.length -1; j++) {
+      
+      string += `${filmes[i].elenco[j]}, `
+    }
+  console.log(string)
+}
